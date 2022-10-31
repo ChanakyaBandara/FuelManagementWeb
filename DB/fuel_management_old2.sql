f3ccc7a7c377403a6b5d9d85e2c7223938a62068
ChanakyaBandara <Chanubandara5@gmail.com>
2022-10-31 19:36:01 +0530

    db  update

--- DB/fuel_management_old2.sql
+++ DB/fuel_management_old2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 31, 2022 at 03:04 PM
+-- Generation Time: Aug 30, 2022 at 02:33 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.1
 
@@ -38,35 +38,13 @@ CREATE TABLE `customer` (
   `lid` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `extends`
+-- Dumping data for table `customer`
 --
 
-CREATE TABLE `extends` (
-  `eid` int(11) NOT NULL,
-  `cid` int(11) NOT NULL,
-  `week` int(11) NOT NULL,
-  `amount` int(11) NOT NULL,
-  `ref` varchar(255) NOT NULL,
-  `approval` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `fuel_arrival`
---
-
-CREATE TABLE `fuel_arrival` (
-  `fa_id` int(11) NOT NULL,
-  `sid` int(11) NOT NULL,
-  `ft_id` int(11) NOT NULL,
-  `amount` int(11) NOT NULL,
-  `timestamp` int(11) NOT NULL,
-  `status` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `customer` (`cid`, `name`, `nic`, `email`, `phone`, `address`, `lid`) VALUES
+(1, 'test customer', '98989833v', 'testcus@gmail.com', 77323232, 'Kandy', 1),
+(2, 'test cus2', '53637778v', 'testcus2@gmail.com', 774777777, 'kurunegala', 3);
 
 -- --------------------------------------------------------
 
@@ -108,8 +86,7 @@ INSERT INTO `login` (`lid`, `username`, `password`, `type`) VALUES
 (1, 'testcus@gmail.com', '$2y$10$ECLxBz3sVc5Nurm8IJZefu1ob5bD5WYnGJx4bViUDpgSP4BZbivsS', 2),
 (2, 'testfuel@gmail.com', '$2y$10$ECLxBz3sVc5Nurm8IJZefu1ob5bD5WYnGJx4bViUDpgSP4BZbivsS', 3),
 (3, 'testcus2@gmail.com', '$2y$10$5n7dCsnPJ/Pva7DPB72xkuhf0UAnRmdL8MM0jLroDNlefeSslBbl.', 2),
-(4, 'testfuel2@gmail.com', '$2y$10$czG5Df96qzGLwCN9BMixu.f3ayTGyg7/TbAvL/PKsd9IE.qOeDeeu', 3),
-(6, 'risiki@gmail.com', '$2y$10$f7EN2QTc8.35SLmcQ7Nha.nC4uBomvmMrnpekopydMsreqNo0LBhC', 3);
+(4, 'testfuel2@gmail.com', '$2y$10$czG5Df96qzGLwCN9BMixu.f3ayTGyg7/TbAvL/PKsd9IE.qOeDeeu', 3);
 
 -- --------------------------------------------------------
 
@@ -123,23 +100,6 @@ CREATE TABLE `record` (
   `vid` int(11) NOT NULL,
   `sid` int(11) NOT NULL,
   `amount` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `special_qr`
---
-
-CREATE TABLE `special_qr` (
-  `sqr_id` int(11) NOT NULL,
-  `cid` int(11) NOT NULL,
-  `qr` int(11) NOT NULL,
-  `purpose` int(11) NOT NULL,
-  `amount` int(11) NOT NULL,
-  `approval` tinyint(1) NOT NULL,
-  `ref` varchar(255) NOT NULL,
-  `qr_code` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -160,6 +120,14 @@ CREATE TABLE `station` (
   `lon` varchar(255) NOT NULL,
   `lid` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `station`
+--
+
+INSERT INTO `station` (`sid`, `name`, `email`, `reg_no`, `city`, `address`, `phone`, `lat`, `lon`, `lid`) VALUES
+(1, 'Siriwaradena Fuel Station', 'testFuel@gmail.com', '112211', 1, 'Kandy', '', '7.286305797505232', '80.62677881897109', 2),
+(2, 'Nirmal filling satation ', 'testfuel2@gmail.com', '12-43', 1, 'Kurunegala', '0774785', '6.786590361512595', '79.96566765010357', 4);
 
 -- --------------------------------------------------------
 
@@ -192,6 +160,13 @@ CREATE TABLE `vehicle` (
   `cid` int(11) NOT NULL,
   `fid` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `vehicle`
+--
+
+INSERT INTO `vehicle` (`vid`, `reg_no`, `brand`, `model`, `engine_no`, `chassis_no`, `qr`, `vtid`, `cid`, `fid`) VALUES
+(1, 'CAW-1234', 'Susuki', 'Wagon R', '1212121', '121212333', 'AAAAAAAAAAA', 2, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -226,13 +201,6 @@ ALTER TABLE `customer`
   ADD KEY `lid` (`lid`);
 
 --
--- Indexes for table `extends`
---
-ALTER TABLE `extends`
-  ADD PRIMARY KEY (`eid`),
-  ADD KEY `cid` (`cid`);
-
---
 -- Indexes for table `fuel_type`
 --
 ALTER TABLE `fuel_type`
@@ -251,13 +219,6 @@ ALTER TABLE `record`
   ADD PRIMARY KEY (`rid`),
   ADD KEY `sid` (`sid`),
   ADD KEY `vid` (`vid`);
-
---
--- Indexes for table `special_qr`
---
-ALTER TABLE `special_qr`
-  ADD PRIMARY KEY (`sqr_id`),
-  ADD KEY `cid` (`cid`);
 
 --
 -- Indexes for table `station`
@@ -297,13 +258,7 @@ ALTER TABLE `vehicle_type`
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `cid` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `extends`
---
-ALTER TABLE `extends`
-  MODIFY `eid` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `cid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `fuel_type`
@@ -315,7 +270,7 @@ ALTER TABLE `fuel_type`
 -- AUTO_INCREMENT for table `login`
 --
 ALTER TABLE `login`
-  MODIFY `lid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `lid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `record`
@@ -324,16 +279,10 @@ ALTER TABLE `record`
   MODIFY `rid` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `special_qr`
---
-ALTER TABLE `special_qr`
-  MODIFY `sqr_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT for table `station`
 --
 ALTER TABLE `station`
-  MODIFY `sid` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `sid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `stock`
@@ -345,7 +294,7 @@ ALTER TABLE `stock`
 -- AUTO_INCREMENT for table `vehicle`
 --
 ALTER TABLE `vehicle`
-  MODIFY `vid` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `vid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `vehicle_type`
@@ -364,23 +313,11 @@ ALTER TABLE `customer`
   ADD CONSTRAINT `customer_ibfk_1` FOREIGN KEY (`lid`) REFERENCES `login` (`lid`);
 
 --
--- Constraints for table `extends`
---
-ALTER TABLE `extends`
-  ADD CONSTRAINT `extends_ibfk_1` FOREIGN KEY (`cid`) REFERENCES `customer` (`cid`);
-
---
 -- Constraints for table `record`
 --
 ALTER TABLE `record`
   ADD CONSTRAINT `record_ibfk_1` FOREIGN KEY (`sid`) REFERENCES `station` (`sid`),
   ADD CONSTRAINT `record_ibfk_2` FOREIGN KEY (`vid`) REFERENCES `vehicle` (`vid`);
-
---
--- Constraints for table `special_qr`
---
-ALTER TABLE `special_qr`
-  ADD CONSTRAINT `special_qr_ibfk_1` FOREIGN KEY (`cid`) REFERENCES `customer` (`cid`);
 
 --
 -- Constraints for table `station`
